bc251289c27f9fcbe324e632b645cbf5624b3624
ptulja <ptulja>
2023-04-12 18:20:16 +0000

    Added the output for case3.c

--- sum/case3.c
+++ sum/case3.c
@@ -29,4 +29,11 @@ int main()
 
 
 /* o/p
- * Error will come beacuse we did assign the datatype to the varablies*/
+ * Error will come beacuse we did assign the datatype to the varablies*
+ *error: ‘num1’ undeclared (first use in this function)
+    6 |      num1=10;
+      |      ^~~~
+case3.c:6:6: note: each undeclared identifier is reported only once for each function it appears in
+case3.c:8:6: error: ‘num2’ undeclared (first use in this function)
+    8 |      num2=20;
+ /
