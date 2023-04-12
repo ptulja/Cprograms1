3d0779a777b13794a0e491e7c11b65ab0b683967
ptulja <ptulja>
2023-04-12 05:54:14 +0000

    Adding the 2 numbers case1

---
+++ sum/sum1.c
@@ -0,0 +1,40 @@
+//write the program on addition of 2 numbers
+#include<stdio.h>
+int main()
+{
+       	//Declaring the variables
+ 
+ 	int num1;
+ 
+  	int num2;
+ 
+  	int result;
+
+ 	// enter the num1 value
+ 
+ 	printf("Enter the num1 value:");
+
+ 	scanf("%d", &num1);
+
+ 	// enter the num2 value
+
+ 	printf("Enter the num2 value:");
+
+ 	scanf("%d", &num2);
+
+ 	//Applying the main logic
+ 
+ 	result=num1+num2;
+
+ 	//print the result of 2 numbers
+
+ 	printf("Result of 2 given number is %d\n", result);
+
+ 	return 0;
+} 
+
+
+/*Enter the num1 value:10
+ *Enter the num2 value:20
+ *Result of 2 given number is 30*/
+ 
