85edaee6633d922e88e650cebc7d86e57ccea015
ptulja <ptulja>
2023-04-19 11:23:20 +0000

    logical oprators

---
+++ logical/case2.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+  //declare the variables
+    int a, b, c, m;
+
+    int r;
+
+    // assign the values to the variable
+
+       a = 10;
+
+       b = 2;
+
+       c = 20;
+
+       m = 2;
+
+   //apply the main logic
+
+   r = (m+=b)/(a&&b)+(c||a)-(a%b);
+
+   //ptint the r value
+
+    printf("%d\n", r);
+
+    //Exit from main funcation
+
+     return 0;
+}
+
+/*output is 5 */
