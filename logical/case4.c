600ed9474032ecdb07300bd9001a4674db7c9a5e
ptulja <ptulja>
2023-04-20 07:42:35 +0000

    logical oprators

---
+++ logical/case4.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main()
+{ 
+  //declare the variables
+
+    int a;
+   
+    int b;
+
+    int c;
+
+    int d;
+
+    int r;
+
+  //assign the values to the variables
+
+    a = 2;
+
+    b = 0;
+
+    c = 3;
+     
+    d = 4;
+
+  //apply the main logic
+
+    r = a+=b+=c+=d+1;
+
+  //print the r value
+
+   printf("%d\n", r);
+
+ //Exit from the main
+ 
+   return 0;
+}   
+/*output is 10*/
+        
